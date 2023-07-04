1e1864d9f61cb9bc720b64d29efdd1c2e4a9e8de
Bonface <bonniewanga34@gmail.com>
2023-07-04 18:14:34 +0300

    completed assignments

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 let num1=31;
 let num2=2;
 let multiply =num1* num2;
-let random =7;
-Number.isInteger(true);
-let max =20;
-Math.max=(7, 20);
+let random=Math.floor(Math.random())+1;
+let max =Math.max=(7, 20);
 let num3=19;
 let num4=15;
 let mod=num3 % num4
